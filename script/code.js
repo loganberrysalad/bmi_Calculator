0b2438224e0ec4e911278938a17b559deacafb33
loganberrysalad <mylifechoicework@gmail.com>
2023-05-23 22:41:45 +0100

    update on restart btn

--- script/code.js
+++ script/code.js
@@ -2,20 +2,28 @@ let weight = document.getElementById("weight");
 let height = document.getElementById("height");
 let result = document.getElementById("result");
 let answer = document.getElementById("answer");
-let btn = document.getElementById("btn");
+let calculate = document.getElementById("calculate");
 
-btn.addEventListener("click", function() {
+restart.addEventListener("click",function(){
+  height.value=""
+  weight.value=""
+  
+})
+
+calculate.addEventListener("click", function() {
   let bmiInput = weight.value / Math.round(height.value * height.value) * 10000;
   result.innerText = bmiInput.toFixed(2);
 
 
-  if(bmiInput < 18.5) {
+
+
+  if(bmiInput <=18.5) {
     answer.innerText = "YOU ARE UNDERWEIGHT 😢"
     
-  } else if (bmiInput > 18.5 && bmiInput < 24.9) {
+  } else if (bmiInput > 18.5 && bmiInput < 24) {
     answer.innerText = "YOU ARE HEALTHY 😁"
     
-  } else if (bmiInput > 25 && bmiInput < 29.9) {
+  } else if (bmiInput > 25 && bmiInput < 29) {
     answer.innerText = "YOU ARE OVERWEIGHT 😕"
     
   } else {
@@ -23,5 +31,4 @@ btn.addEventListener("click", function() {
   }
   
 })
-
 
